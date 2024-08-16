0ea08a9b4713704ebc230f66e974c3ace04beb4b
mayrang <pkss0626@naver.com>
2024-08-16 14:14:50 +0700

    delete dist for test

--- app/page.js
+++ app/page.js
@@ -60,9 +60,9 @@ const HomePage = () => {
               });
 
               //이동거리가 5m미만이면 안바뀜
-              if (dist < 0.001) {
-                updateFlag = false;
-              }
+              // if (dist < 0.001) {
+              updateFlag = false;
+              // }
             }
             if (updateFlag) {
               setcoords(new_record);
