3a3e3130061f307da97996dee85b0311372b4b77
mayrang <pkss0626@naver.com>
2024-08-21 16:01:12 +0700

    plus icon 수정

--- components/icons/Plus.jsx
+++ components/icons/Plus.jsx
@@ -5,12 +5,11 @@ export default function Plus() {
       viewBox="0 0 24 24"
       width="32"
       height="32"
-      class="main-grid-item-icon"
       fill="none"
       stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
     >
       <line x1="12" x2="12" y1="5" y2="19" />
       <line x1="5" x2="19" y1="12" y2="12" />
