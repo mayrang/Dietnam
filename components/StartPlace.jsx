5f21ac7b5c624fe55c411360904e1ff312dc71fa
giljihun <accc45@outlook.com>
2024-08-21 19:35:45 +0700

    modify(StartPlace) : 🔥 Start Place 🔥 로 변경

--- components/StartPlace.jsx
+++ components/StartPlace.jsx
@@ -5,7 +5,7 @@ export default function StartPlace() {
     <div className="flex w-full items-center gap-2">
       <Marker color="green" />
       <div className="w-full py-2 px-2 flex-1 border-solid border-2 rounded-md border-gray-300 shadow">
-        Start Place
+      🔥 Start Place 🔥
       </div>
     </div>
   );
