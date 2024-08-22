6ad9a4766d8afd0f96a94e33ccc783c2e6a9fd5b
giljihun <accc45@outlook.com>
2024-08-22 09:45:27 +0700

    modify(carouselcard, start place) : shadowing, delete 🔥

--- components/StartPlace.jsx
+++ components/StartPlace.jsx
@@ -5,7 +5,7 @@ export default function StartPlace() {
     <div className="flex w-full items-center gap-2">
       <Marker color="green" />
       <div className="w-full py-2 px-2 flex-1 border-solid border-2 rounded-md border-gray-300 shadow">
-      🔥 Start Place 🔥
+      Start Place
       </div>
     </div>
   );
