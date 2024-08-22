72989a0abea81313f04651815dfa6794d105a0dd
mayrang <pkss0626@naver.com>
2024-08-22 15:36:51 +0700

    start marker 부활

--- components/StartPhoto.jsx
+++ components/StartPhoto.jsx
@@ -63,9 +63,9 @@ export default function StartPhoto() {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          width="50"
-          height="50"
-          fill="white"
+          width="36"
+          height="36"
+          fill="none"
           stroke="currentColor"
           strokeLinecap="round"
           strokeLinejoin="round"
@@ -85,9 +85,10 @@ export default function StartPhoto() {
     <div className="flex flex-col gap-4 items-center">
       <StartPlace />
       <div className="flex w-full  items-center gap-2">
+        <div className="size-8 " />
         <button
           onClick={() => setIsStartCamera(true)}
-          className=" border-2   cursor-pointer w-full h-24 rounded-md border-solid border-gray-300 flex justify-center items-center shadow"
+          className=" border-2   cursor-pointer w-full h-20 rounded-md border-solid border-gray-300 flex justify-center items-center shadow"
         >
           <CameraIcon />
         </button>
