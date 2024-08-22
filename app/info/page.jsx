5aab13f29cab9a9454b9606d5625dbd58a04782e
giljihun <accc45@outlook.com>
2024-08-22 09:13:25 +0700

    modify(info) : modified animations

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -7,15 +7,23 @@ export default function InfoPage() {
     <div className="w-full h-[calc(100svh-92px)] flex flex-col items-center justify-center">
       <div className="flex flex-col flex-1 items-center justify-center">
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
+          className="flex items-center gap-6"
+          initial={{ opacity: 0, scale: 0}}
+          animate={{ opacity: 1, scale: 1.5 }}
+          transition={{ duration: 2.0 }}
         >
           <Image
             width={120}
             height={120}
             src="/hanbat-fimo.png"
             alt="hanbat univ. and fimo logo"
+          />
+          <Image
+            width={120}
+            height={120}
+            src="/logo.png"
+            className="rounded-xl overflow-hidden transition-opacity"
+            alt="Dietnam logo"
           />
         </motion.div>
         <motion.h2
@@ -26,29 +34,18 @@ export default function InfoPage() {
         >
           Dietnam
         </motion.h2>
+
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          <Image
-            width={120}
-            height={120}
-            src="/logo.png"
-            className="rounded-xl overflow-hidden transition-opacity"
-            alt="Dietnam logo"
-          />
-        </motion.div>
-      </div>
-      <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 2 }}
-        className="my-10"
+        className="my-10 absolute bottom-0 -translate-x-1/2 left-1/2"
       >
-        <div className="font-medium text-2xl">Developed by Gil, Ha, Park</div>
-        <div className="font-medium text-2xl">Powered by Wemap</div>
+        <div className="font-serif text-lg">Powered by Wemap</div>
+        <div className="font-serif text-lg">Developed by Gil, Ha, Park</div>
       </motion.div>
+
+      </div>
     </div>
   );
 }
