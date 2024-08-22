a9aebd2ba33c6a08de757001da4905943025f2ac
giljihun <accc45@outlook.com>
2024-08-22 10:32:21 +0700

    modify(components) : finishPhoto rounded

--- components/CheckFinishPhoto.jsx
+++ components/CheckFinishPhoto.jsx
@@ -63,8 +63,7 @@ export default function CheckFinishPhoto() {
       <InputRouteName />
       <FinishPlace />
       <div className="flex w-full  items-center gap-2">
-        <div className="size-8 " />
-        <div className=" border-2   cursor-pointer w-full h-[250px] border-solid  rounded-md border-gray-300 shadow flex justify-center items-center overflow-hidden">
+        <div className="cursor-pointer w-full h-[250px] rounded-3xl shadow-xl flex justify-center items-center overflow-hidden">
           <Image
             src={finishImage}
             className="w-full h-full object-cover"
@@ -77,7 +76,7 @@ export default function CheckFinishPhoto() {
 
       <button
         onClick={handleSave}
-        className="px-7 rounded-md font-bold cursor-pointer py-2 border-2 border-solid border-black "
+        className="px-7 rounded-md font-bold cursor-pointer py-2 border-2 border-solid border-gray "
       >
         Save
       </button>
--- components/FinishPlace.jsx
+++ components/FinishPlace.jsx
@@ -4,7 +4,7 @@ export default function FinishPlace() {
   return (
     <div className="flex w-full items-center gap-2">
       <Marker color="blue" />
-      <div className="w-full py-2 px-2 flex-1 border-solid border-2 rounded-md border-gray-300 shadow">
+      <div className="w-full py-2 px-2 flex-1 text-2xl font-extrabold text-gray-500">
         Finish Place
       </div>
     </div>
