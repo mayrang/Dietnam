4ea8b5a6ad7c2df54035fdb5a6ed11d3e4a816bf
mayrang <pkss0626@naver.com>
2024-09-03 15:08:57 +0900

    test 콘솔 삭제

--- app/making/page.jsx
+++ app/making/page.jsx
@@ -9,7 +9,6 @@ import { useCurrentMarkerStore, useMapStore } from "../../store/making";
 import useCurrentPosition from "../../hooks/useCurrentPosition";
 
 const MakingPage = () => {
-  console.log(1);
   const { currentMarker, setCurrentMarker } = useCurrentMarkerStore();
   const { map, setMap } = useMapStore();
 
